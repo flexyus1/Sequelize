b9cafc25dd516a2656024ba1d903ce3e7621f394
Leonildo Camilo <135379433+flexyus1@users.noreply.github.com>
2024-01-11 21:07:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Codigo necessario para rodar a aplicação express, conectar no banco de dados e outras configurações
 const express = require('express')
 const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
@@ -16,7 +17,7 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
-
+//-------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {
   res.render('home')
 })
